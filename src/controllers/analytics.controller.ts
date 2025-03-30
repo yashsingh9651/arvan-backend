2d813b7bdbd8c9413aef29b32137351e27ba32e8
Aditya Bansal <adityabansalsafe@gmail.com>
2025-03-30 14:42:02 +0530

    add new arrivals

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -44,6 +44,91 @@ const getTopProducts = async (req: Request, res: Response, next: NextFunction) =
   res.status(HttpStatusCodes.OK).json({ success: true, products });
 };
 
+
+const getBesSellers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const limit = parseInt(req.query.limit as string) || 5;
+
+  const topProducts = await prisma.orderItem.groupBy({
+    by: ["productVariantId"],
+    _sum: {
+      quantity: true,
+      priceAtOrder: true,
+    },
+  });
+  // Sort and limit
+  const limitedProducts = topProducts
+    .sort((a, b) => (b._sum.quantity || 0) - (a._sum.quantity || 0))
+    .slice(0, limit);
+
+  // Fetch product variants and their products in a single query
+  const productVariants = await prisma.productVariant.findMany({
+    where: {
+      id: { in: limitedProducts.map((item) => item.productVariantId) },
+    },
+    include: {
+      color: {
+        include: {
+          product: {
+            include: {
+              category: true,
+            },
+          },
+          assets: {
+            take: 1,
+          },
+        },
+      },
+    },
+  });
+  const products = productVariants.map((variant) => {
+    return {
+      productid: variant.color?.product?.id || "",
+      img: variant.color.assets[0].asset_url, 
+      name: variant.color?.product?.name ,
+      price: variant.color.product.price,
+      category: variant.color?.product.category.name , // Assuming category field exists
+      discount: variant.color.product.discountPrice, // Assuming discount field exists
+    };
+  });
+
+  res.status(HttpStatusCodes.OK).json({ success: true, products });
+};
+
+const newArrivals = async (req: Request, res: Response, next: NextFunction) => {
+  const limit = parseInt(req.query.limit as string) || 5;
+
+  const newProducts = await prisma.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: limit,
+    include: {
+      category: true,
+      assets: {
+        take: 1,
+      },
+    },
+  });
+
+  const products = newProducts.map((product) => ({
+    id: product.id,
+    name: product.name,
+    img: product.assets[0].asset_url,
+    price: product.price,
+    discountPrice: product.discountPrice,
+    category: product.category.name,
+  }));
+
+  res.status(HttpStatusCodes.OK).json({ success: true, products });
+};
+
+
 export const AnalyticsController = {
   getTopProducts,
+  getBesSellers,
+  newArrivals,
 };
